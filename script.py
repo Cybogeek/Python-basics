17d6b738b23609595999a69716dd37de35b5a722
cybogeek <cybokeek2017@gmail.com>
2025-06-04 23:34:37 +0530

    update code

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
-
+# code by Sukant @ Cybogeek
 if __name__ == '__main__':
-
+    # input for First Number
     num1=input('Enter the first number: ')
+    #input for Second Number
     num2=input('Enter the second number: ')
-
+    # Validation for Num 1 & Num 2, make sure the input is a Number
     if num1.isdigit() & num2.isdigit():
+    # Print Addition, Subtraction, Multiplication, and Divison result. Using int() for type Casting String into Integers.
      print(f"\nAddition: {int(num1)+int(num2)}")
      print(f"Subtraction: {int (num1) - int(num2)}")
      print(f"Multiplication: {int(num1) * int(num2)}")
--- script1.py
+++ script1.py
@@ -1,5 +1,8 @@
+# Code by Sukant @ Cybogeek
 if __name__ == '__main__':
-    fname = input('Enter your first name: ')
-    lname = input('Enter your last name: ')
+    # using input to get the First Name from User, Store it in variable- fName
+    fName = input('Enter your first name: ')
+    # using input to get the Last Name from User, Store it in variable- lName
+    lName = input('Enter your last name: ')
 
-    print(f"\nHello {fname} {lname}! Welcome to the Python Program.")
+    print(f"\nHello {fName} {lName}! Welcome to the Python Program.")
